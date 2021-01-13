4a9e3897faa9e59b7c9b1dc7c60684aa6a4c3608
nativearth-afk <71740499+nativearth-afk@users.noreply.github.com>
2021-01-13 19:37:29 +0800

    Add files via upload

---
+++ parameters.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"D:\\test.avi"		
+#define OPENPOSE_VIDEO		"D:\\openposetest.mp4"		
+
+//#define INPUT_METHOD									//0: image 1:video 2:camera
+#pragma once
